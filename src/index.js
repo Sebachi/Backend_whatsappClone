bbb74d0e6c4ed67a9493c877d3210d166e6a0ff1
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-07-04 00:34:09 -0500

    Changes chatgpt

--- src/index.js
+++ src/index.js
@@ -10,7 +10,12 @@ server.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', '*')
     res.header('Access-Control-Allow-Methods', '*')
     next()
-})
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+      } else {
+        next();
+      }
+    });
 
 server.use(router);
 server.use(middlewares)
